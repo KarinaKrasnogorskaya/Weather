af0ed0477896180d4145b765636d8af4831c8b18
KarinaKR00 <kr_karina00@mail.ru>
2023-03-12 16:24:38 +0300

    forecast card

--- Weather/Components/ForecastCard.swift
+++ Weather/Components/ForecastCard.swift
@@ -10,7 +10,15 @@ import SwiftUI
 struct ForecastCard: View {
     var forecast: Forecast
     var forecastPeriod: ForecastPeriod
-    var isActive: Bool = true
+    var isActive: Bool {
+        if forecastPeriod == ForecastPeriod.hourly {
+            let isThisHour = Calendar.current.isDate(.now, equalTo: forecast.date, toGranularity: .hour)
+            return isThisHour
+        } else {
+            let isToday = Calendar.current.isDate(.now, equalTo: forecast.date, toGranularity: .day)
+            return isToday
+        }
+    }
     
     var body: some View {
         ZStack {
@@ -22,8 +30,38 @@ struct ForecastCard: View {
                 .overlay {
                     //MARK: Card Border
                     RoundedRectangle(cornerRadius: 30)
-                        .strokeBorder()
+                        .strokeBorder(.white.opacity(isActive ? 0.5 : 0.2))
+                        .blendMode(.overlay)
                 }
+                .innerShadow(shape: RoundedRectangle(cornerRadius: 30), color: .white.opacity(0.25), lineWight: 1, offsetX: 1, offsetY: 1, blur: 0, blendMode: .overlay)
+            
+            //MARK: Content
+            
+            VStack(spacing: 16) {
+                //MARK: Forecast Date
+                Text(forecast.date, format: forecastPeriod == ForecastPeriod.hourly ? .dateTime.hour() : .dateTime.day())
+                    .font(.subheadline.weight(.semibold))
+                
+                VStack(spacing: -4) {
+                    //MARK: Forecast Small Icon
+                    Image("\(forecast.icon) small")
+                    
+                    //MARK: Forecast Probability
+                    
+                    Text(forecast.probability, format: .percent)
+                        .font(.footnote.weight(.semibold))
+                        .foregroundColor(Color.probabilityText)
+                        .opacity(forecast.probability > 0 ? 1 : 0)
+                }
+                .frame(height: 42)
+                
+                //MARK: Forecast Temperature
+                Text("\(forecast.temperature)°")
+                    .font(.title3)
+            }
+            .padding(.horizontal, 8)
+            .padding(.vertical, 16)
+            .frame(width: 60, height: 146)
         }
     }
 }
